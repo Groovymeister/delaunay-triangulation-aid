6c7ca64c16198e554215e6e36684036ede0bde62
Groovymeister <megajohn92@gmail.com>
2024-10-30 18:37:45 -0400

    Made some more progress, done for now

--- delaunay.py
+++ delaunay.py
@@ -1,4 +1,5 @@
-from utils import angleof, is_point_in_circle
+from utils import angleof, is_point_in_circle, edges_to_points
+import math
 
 class Point:
     def __init__(self, x, y):
@@ -23,8 +24,22 @@ def lr_edge(left, right):
     right_bottom = min(right, key=lambda edge: (edge.p1.y, edge.p2.y))
     return Edge(left_bottom.p1, right_bottom.p1)
 
-def get_candidate(base_edge, edges, right):
-    pass
+def get_candidates(base_edge, points, is_right):
+    base_point = base_edge.p2 if is_right else base_edge.p1
+    reference_point = base_edge.p1 if is_right else base_edge.p2
+    candidates = []
+    
+    for point in points:
+        if point == base_point:
+            continue
+        
+        angle = angleof(reference_point, base_point, point)
+        
+        if angle > 0:
+            candidates.append((point, angle))
+    
+    candidates.sort(key=lambda x: x[1])
+    return [point for point, angle in candidates]
 
 def is_delaunay(base_edge, candidate, triangulation):
     for edge in triangulation:
@@ -32,15 +47,46 @@ def is_delaunay(base_edge, candidate, triangulation):
             return False
     return True
 
-def remove_edges_with_point(triangulation, point):
-    return [edge for edge in triangulation if edge.p1 != point and edge.p2 != point]
+def criteria_met(lr_edge, pt):
+    angle = angleof(lr_edge.p1, lr_edge.p2, pt)
+    if not (0 < angle < math.pi):
+        return False
+
+    return True
 
 def merge(left, right):
     edges = []
     base_lr_edge = lr_edge(left, right)
     edges.append(base_lr_edge)
 
-    
+    while True:
+
+        left_candidate = None
+        right_candidate = None
+        right_candidates = get_candidates(base_lr_edge, edges_to_points(right), True)
+        left_candidates = get_candidates(base_lr_edge, edges_to_points(left), False)
+        
+        while not right_candidate and len(right_candidates) > 0:
+            if angleof(lr_edge.p1, lr_edge.p2, right_candidate) < math.pi and is_point_in_circle(base_lr_edge.p1, base_lr_edge.p2, right_candidates[0], right_candidates[1]):
+                right_candidate = right_candidates[0]
+            elif angleof(lr_edge, right_candidate) < math.pi:
+                right_candidates.pop(0) # second holds but first doesnt, remove RR edge from potential candidates
+            else:
+                break # If the first criteria does not hold, we do not select a right candidate
+
+        while not left_candidate and len(left_candidates) > 0:
+            if angleof(lr_edge.p1, lr_edge.p2, left_candidate) < math.pi and is_point_in_circle(base_lr_edge.p1, base_lr_edge.p2, left_candidates[0], left_candidates[1]):
+                left_candidate = left_candidates[0]
+            elif angleof(lr_edge.p1, lr_edge.p2, left_candidate) < math.pi:
+                left_candidates.pop(0) # second holds but first doesnt, remove LL edge from potential candidates
+            else:
+                break # If the first criteria does not hold, we do not select a left candidate
+
+        if not left_candidate and not right_candidate:
+            break
+
+        # TODO: IMPLEMENT CASES WHERE ONE CANDIDATE AND BOTH CANDIDATES ARE SUBMITTED.
+
 
     return edges
 
--- utils.py
+++ utils.py
@@ -3,8 +3,10 @@ import math
 def remove_edges_with_point(triangulation, point):
     return [edge for edge in triangulation if edge.p1 != point and edge.p2 != point]
 
-def angleof(p1, p2):
-    return math.atan2(p2.y - p1.y, p2.x - p1.x)
+def angleof(p1, p2, p3):
+    dx1, dy1 = p2.x - p1.x, p2.y - p1.y
+    dx2, dy2 = p3.x - p2.x, p3.y - p2.y
+    return math.atan2(dy2, dx2) - math.atan2(dy1, dx1)
 
 def is_point_in_circle(a, b, c, d):
     ax, ay = a.x - d.x, a.y - d.y
@@ -19,3 +21,13 @@ def is_valid(base_edge, candidate, triangulation):
         if is_point_in_circle(base_edge.p1, base_edge.p2, candidate, edge.p2 if edge.p1 == candidate else edge.p1):
             return False
     return True
+
+def edge_to_points(edge):
+    return [edge.p1, edge.p2]
+
+def edges_to_points(edges):
+    points = set()
+    for edge in edges:
+        points.add(edge.p1)
+        points.add(edge.p2)
+    return list(points)
