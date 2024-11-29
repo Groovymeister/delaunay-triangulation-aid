6495c33df4c856cd0495a005c934a742b7dbdf88
Groovymeister <megajohn92@gmail.com>
2024-11-29 13:28:49 -0500

    gotta push to test this >:(

--- docs/triangulate.py
+++ docs/triangulate.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS 
+from flask_cors import CORS
 import numpy as np
 from scipy.spatial import Delaunay
 import os
+from utils import Point, Edge
+from delaunay import delaunay
 
 app = Flask(__name__)
 CORS(app)
@@ -10,15 +12,19 @@ CORS(app)
 @app.route('/api/triangulate', methods=['POST'])
 def triangulate():
     data = request.get_json()
-    points = data.get('points', [])
+    points_data = data.get('points', [])
     
-    if not points:
+    if not points_data:
         return jsonify({"error": "No points provided"}), 400
 
-    points_array = np.array(points)
-    triangulation = Delaunay(points_array)
+    points = [Point(p['x'], p['y']) for p in points_data]
 
-    return jsonify(triangles=triangulation.simplices.tolist())
+    triangulation = delaunay(points)
+
+    triangles = [{"p1": {"x": edge.p1.x, "y": edge.p1.y}, 
+                  "p2": {"x": edge.p2.x, "y": edge.p2.y}} for edge in triangulation]
+
+    return jsonify(triangles=triangles)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
