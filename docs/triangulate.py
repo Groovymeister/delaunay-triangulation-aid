40931fd7f14620f30303c770cacff08dee6fc6fc
Groovymeister <megajohn92@gmail.com>
2024-11-29 13:01:14 -0500

    seeing if modifying cors helps

--- docs/triangulate.py
+++ docs/triangulate.py
@@ -5,7 +5,7 @@ from scipy.spatial import Delaunay
 import os
 
 app = Flask(__name__)
-CORS(app)  
+CORS(app, origins=["https://groovymeister.github.io/delaunay-triangulation-aid/"])
 
 @app.route('/api/triangulate', methods=['POST'])
 def triangulate():
